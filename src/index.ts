a498f22b55df7c9eb11150e2a5f958b27a7d095e
pooja <pooja357devi@gmail.com>
2024-03-10 17:40:02 +0530

    gq server setup

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import express from 'express'
+import { ApolloServer } from '@apollo/server'
+import {expressMiddleware} from '@apollo/server/express4'
+
+
+
+async function init() {
+    const app = express()
+
+// /
+
+const port = process.env.PORT || 8000
+app.use(express.json())
+
+//  create graphql server
+const gqlServer = new ApolloServer({
+    typeDefs: `
+    type Query { 
+        hello: String, 
+        say(name: String): String
+    }`, 
+    resolvers: {
+        Query: { 
+            hello: () => `Hey there, I am gq server`, 
+            say: (_, {name} : {name: string}) => `Hey ${name}, How Are You!`
+        }
+    }
+})
+
+await gqlServer.start()
+
+
+
+app.get('/', (req,res) => { 
+    res.json({message: 'server is up and running'})
+})
+
+
+app.use('/graphql', expressMiddleware(gqlServer))
+
+app.listen(port, () => { 
+    console.log('running on 8000')
+})
+    
+}
+
+init()
